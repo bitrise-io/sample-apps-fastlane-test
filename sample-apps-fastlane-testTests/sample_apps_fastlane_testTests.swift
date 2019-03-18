ff3ec7b98bfd821eaf0c39937b501cc67cbc527f
Akos Birmacher <akos.birmacher@bitrise.io>
2019-03-18 14:18:00 +0100

    Initial Commit

---
+++ sample-apps-fastlane-testTests/sample_apps_fastlane_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  sample_apps_fastlane_testTests.swift
+//  sample-apps-fastlane-testTests
+//
+//  Created by Akos Birmacher on 2019. 03. 18..
+//  Copyright © 2019. Bitrise. All rights reserved.
+//
+
+import XCTest
+@testable import sample_apps_fastlane_test
+
+class sample_apps_fastlane_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
